3b8343f2e5eb149053ac51013b88a98448692388
witsarutsupsi <79503806+witsarutsupsi@users.noreply.github.com>
2021-12-20 10:17:26 +0700

    Update simulator.vue

--- src/components/simulator.vue
+++ src/components/simulator.vue
@@ -2,7 +2,6 @@
 <div>
     Hi Hi Hi
   <unity src="static/Build/VK Build.json" width="1000" height="600" unityLoader="static/Build/UnityLoader.js"></unity>  
-   <div v-text="ImageBase64">5</div>
 </div>
 </template>
 
@@ -14,17 +13,12 @@ export default {
   components: {
       Unity,
     },
-    data: {
-          ImageBase64: 'hello'
-    },
     methods: {
-    ImageStream(value) {
-      //document.getElementById("ImageBase64") = value;
-      //this.ImageBase64 = value;
-    
+        onClick () {
+            this.$refs.myInstance.message("CameraVK", "KanomchanLinearDirec", 0.1)
+        }
+        //this.$refs.myInstance.message("CameraVK", "KanomchanLinearDirec", 0.1);
     }
-    //this.$refs.myInstance.message("CameraVK", "KanomchanLinearDirec", 0.1);
-  }
 };
 </script>
 
