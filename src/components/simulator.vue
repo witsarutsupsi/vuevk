1c8286ea2cb6b9024b800026362220c92e505f26
witsarutsupsi <79503806+witsarutsupsi@users.noreply.github.com>
2021-12-21 10:22:24 +0700

    Update simulator.vue

--- src/components/simulator.vue
+++ src/components/simulator.vue
@@ -1,30 +1,26 @@
 <template>
 <div>
     Hi Hi Hi
-  <script type="javascript" src="../static/Build/UnityLoader.js"></script>
-  <unity src="static/Build/VK Build.json" width="1000" height="600" unityLoader="static/Build/UnityLoader.js"></unity>
+  <unity ref="gameInstance" src="static/Build/VK Build.json" width="1000" height="600" unityLoader="static/Build/UnityLoader.js"></unity>
   <div id="ImageBase64" ref="ImageBase64">12</div>
   <button v-on:click="onClick()">Click Forward</button>
 </div>
 </template>
 
 <script>
-//import Unity from 'vue-unity-webgl'
-import { Unity, Message } from 'vue-unity-webgl';
+import Unity from 'vue-unity-webgl'
 
 export default {
     name: "component-name",
     components: {
         Unity,
     },
-
     methods: {
         //ImageStream (value){
             //this.$refs.ImageBase64.innerText = value;    
         //},
         onClick() {
-            Message('CameraVK', 'KanomchanLinearDirec', '0.1');
-            //this.$refs.gameInstance.SendMessage("CameraVK", "KanomchanLinearDirec", "0.1"));
+            this.$refs.gameInstance.SendMessage("CameraVK", "KanomchanLinearDirec", "0.1"));
             //this.$refs.ImageBase64.innerText = 20;   
         }
 
