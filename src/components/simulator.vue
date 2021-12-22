a2f9407a52becbec628bd85beaa0528c70deb73c
witsarutsupsi <79503806+witsarutsupsi@users.noreply.github.com>
2021-12-22 13:06:11 +0700

    Update simulator.vue

--- src/components/simulator.vue
+++ src/components/simulator.vue
@@ -37,9 +37,7 @@ export default {
             this.$refs.gameInstance.message("CameraVK", "KanomchanLinearDirec", "0"); 
         },
         onClickImgStream() {
-            ImgStream().onProgress = function (gameInstance, progress) {
-                this.$refs.ImageBase64.innerText = progress;
-            };
+            this.$refs.ImageBase64.innerText = ImgStream().onProgress;
             //this.$refs.ImageBase64.innerText = "" + (this.$refs.gameInstance.message("CameraVK", "ImgStream")); 
         }
     }
